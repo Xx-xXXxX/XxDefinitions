5553754853339d3e2eb56a0028a3f6f0e284e7fd
Xx-xXXxX <liuzeyu20050612@163.com>
2022-03-12 17:06:22 +0800

    IR

---
+++ IRandom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XxDefinitions
+{
+	/// <summary>
+	/// 对随机数的接口
+	/// </summary>
+	public interface IRandom
+	{
+
+	}
+}
